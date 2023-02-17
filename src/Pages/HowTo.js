325bc82f3d5363452e01686ddc0581da6be5edfb
Sofie Ventzel <115930783+sofie-ventzel@users.noreply.github.com>
2023-02-17 15:21:23 +0000

    added HowTo page with basic function

---
+++ src/Pages/HowTo.js
@@ -0,0 +1,15 @@
+//how to watch birds 101, a page describing what to look out for
+import Header from '../Componants/Header'
+import Footer from "../Componants/Footer"
+
+function HowTo() {
+    return (
+        <>
+            <Header />
+            
+            <Footer />
+        </>
+    )
+}
+
+export default HowTo
