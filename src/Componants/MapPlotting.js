baa349bb26c9737c13b61b6ec3328209c1a6e752
BrianMillie <daniel.shipp@uk.verizon.com>
2023-02-17 14:42:38 +0000

    pushing for fix

--- src/Componants/MapPlotting.js
+++ src/Componants/MapPlotting.js
@@ -23,6 +23,7 @@ function MapPlotting() {
 
     return (
         <div>
+            <h1>{state.birdName}</h1>
             <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} id="map">
                 <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
