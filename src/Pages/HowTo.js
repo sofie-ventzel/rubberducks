11400b7d3d361abc203646a0d8fc558dbafcb77d
Sofie Ventzel <115930783+sofie-ventzel@users.noreply.github.com>
2023-02-17 15:53:59 +0000

    readjusted youtube position on the homepage

--- src/Pages/HowTo.js
+++ src/Pages/HowTo.js
@@ -1,6 +1,6 @@
 //how to watch birds 101, a page describing what to look out for
-import Header from '../Componants/Header'
-import Footer from "../Componants/Footer"
+import Header from '../Componants/Header';
+import Footer from '../Componants/Footer'
 
 function HowTo() {
     return (
