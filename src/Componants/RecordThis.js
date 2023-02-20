5ac1be0d3b8334d8e128d4120465f92998723dd9
Sofie Ventzel <115930783+sofie-ventzel@users.noreply.github.com>
2023-02-20 14:53:09 +0000

    added record this component

---
+++ src/Componants/RecordThis.js
@@ -0,0 +1,10 @@
+function RecordThis() {
+    return (
+        <>
+        <h2>What to record</h2>
+        <p>When making an entry, the most useful things to record include the species name, the amount of birds of that species, the date and the place.</p>
+        <p>Additional information like the age, the sex, the direction of flight or any curious behavior adds value to the observation.</p>
+        </>
+    )
+}
+export default RecordThis;
--- src/Pages/HowTo.js
+++ src/Pages/HowTo.js
@@ -1,15 +1,16 @@
 //how to watch birds 101, a page describing what to look out for
 import Header from '../Componants/Header';
-import Footer from '../Componants/Footer'
+import Footer from '../Componants/Footer';
+import RecordThis from '../Componants/RecordThis';
 
 function HowTo() {
     return (
         <>
             <Header />
-            
+            <RecordThis />
             <Footer />
         </>
     )
 }
 
-export default HowTo
+export default HowTo;
