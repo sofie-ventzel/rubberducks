fc531ee238f18fb6e9c0dccc9fde17d9ce506b29
Sofie Ventzel <115930783+sofie-ventzel@users.noreply.github.com>
2023-02-20 15:46:07 +0000

    added article on beginner birdwatching

--- src/Pages/HowTo.js
+++ src/Pages/HowTo.js
@@ -1,12 +1,11 @@
 //how to watch birds 101, a page describing what to look out for
-import Header from '../Componants/Header';
+
 import Footer from '../Componants/Footer';
 import RecordThis from '../Componants/RecordThis';
 
 function HowTo() {
     return (
         <>
-            <Header />
             <RecordThis />
             <Footer />
         </>
