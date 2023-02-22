5f493795f918ad2c4e1b765025ed269efbd1dc79
BrianMillie <daniel.shipp@uk.verizon.com>
2023-02-22 09:01:13 +0000

    another commit

--- src/Componants/Birds.js
+++ src/Componants/Birds.js
@@ -19,7 +19,7 @@ function Birds() {
                 <div style={{ alignItems: 'stretch' }} >
                     <img style={{ flexDirection: 'JustifyContent', marginTop: '1%', marginBottom: '1%' }} src="/HomeImage.jpg" alt="Photo of 2 keen birdwatchers" />
                 </div>
-                <div style={{ display: 'flex', position: 'absolute', right: '20%', left: '20%', bottom: '15%', }}>
+                <div style={{ display: 'flex', position: 'absolute', right: '20%', left: '20%', bottom: '20%', }}>
                     <div style={{ display: 'flex', flex: 'auto', opacity: '0.6' }}>
                         <Select onChange={handleChange} placeholder="Start typing then select your bird" options={options} styles={{ width: 'maxwidth', opacity: '0.5' }} />
                         <NavLink to="/findings"
--- src/Componants/RecordThis.js
+++ src/Componants/RecordThis.js
@@ -1,6 +1,6 @@
 function RecordThis() {
     return (
-        <>
+        <div class="flex justify-center items-center">
             <div class="container">
                 <h2 class="bg-rosyPink text-xl font-bold">How to get into birdwatching</h2>
                 <h3 class="bg-rosyPink">7 top tips</h3>
@@ -9,38 +9,39 @@ function RecordThis() {
                 <h3>Where do I start birdwatching?</h3>
                 <p>Start wherever you are! Birds are everywhere if you take the time to notice them. Begin by trying gardens, parks, local reserves or woods. Find a spot you know well, ideally somewhere birds are easy to see – with feeders, perhaps. This is where you can get your eye in and become familiar with a good selection of species.</p>
 
-                <img src="/statue.jpg"></img>
+                <img className="h-40 w-40" src="/statue.jpg"></img>
                 <h3>How do I find the birds?</h3>
                 <p>Use your full range of senses. Your peripheral vision is highly sensitive to movement (an adaptation our ancestors used to detect both predators and prey), so try to tune into what’s going on in the corners of your eyes. Listen, too – you’ll need to stop moving about to do so properly, though you can hear even the quietest calls if you listen hard enough. Loud bursts of chattering song or clicking calls close by are warnings that birds have seen you – but can you see them?</p>
 
-                <img src="/ChallengePoster.jpg"></img>
+                <img className="h-40 w-40" src="/ChallengePoster.jpg"></img>
                 <h3>How do I learn to identify birds?</h3>
                 <p>There’s no substitute for experience, so just get out there and practice! Joining an organised session or asking a regular birder to help you is really useful – they will help you see more, and point out important features in real time. This is much more effective and memorable than learning from books or websites, though the latter becomes more useful as your own ability grows.
                     If you're able to take photographs or sound recordings, share them on social media sites and ask for identification help. The majority of people are more than happy to help a beginner get to grips with the different species.</p>
 
-                <img src="/BirdWatchingKit.jpg"></img>
+                <img className="h-40 w-40" src="/BirdWatchingKit.jpg"></img>
                 <h3>What kit do I need?</h3>
                 <p>Binoculars not only help you see details you’ll need to identify birds – they allow you to enter their world, and see individual feathers shifting as a bird turns its head; the puff of steamy breath as it sings on a cool morning; and the precise movements of beak and claw. Be warned: these exquisite details are how birdwatchers get hooked.
                     Take a notebook and pencil to record your experiences as they happen – don’t assume you’ll remember everything. You may also wish to buy a field guide such as the tough, waterproof charts for different habitats from the Field Studies Council.</p>
 
-                <img src="/Sunset.jpg"></img>
+                <img className="h-40 w-40" src="/Sunset.jpg"></img>
                 <h3>When should I go?</h3>
                 <p>There’s plenty to occupy the birdwatcher in each of the four seasons: spring for newly arrived migrants, peak birdsong, courtship and nesting behaviour; summer for parents and fledglings intent on feeding; autumn for gathering migrants; and winter for intense action around feeding stations, invasions of visitors from further north, late afternoon roosts, vast flocks of waterfowl, and starling murmurations.
                     Birds have daily routines as we do – and these are most predictable at the beginning and the end of the day, so these are usually the best times to watch. </p>
 
-                <img src="/Notepad.jpg"></img>
+                <img className="h-40 w-40" src="/Notepad.jpg"></img>
                 <h3>What if I get stuck?</h3>
                 <p>Everyone does! If you see a mystery bird, note down as many details as you can including its appearance, song, location and behaviour. If you can take a photo (even if it’s not a good one) that will help. Use the information to consult a field guide, share with a local bird group or social media. Don’t be afraid to ask questions.</p>
 
-                <img src="/Hide.jpg"></img>
+                <img className="h-40 w-40" src="/Hide.jpg"></img>
                 <h3>How do I use a hide?</h3>
                 <p>A hide full of people armed with expensive-looking kit can be intimidating, but don’t be put off. Enter and close the door quietly. If you’re not sure what birds to expect, look for a log book or board with recent sightings. Find a seat and a window. It’s fine to talk quietly, and to ask people what they have seen – most will be happy to share, and to point out when they see something interesting. When it’s time to leave, do so quietly, but do add any new sightings to the log.</p>
+
+                <img className="h-40 w-40" src="/Untitled.png" alt="Yellow duck" />
+                <h2>What to record</h2>
+                <p>When making an entry, the most useful things to record include the species name, the amount of birds of that species, the date and the place.
+                    Additional information like the age, the sex, the direction of flight or any curious behavior adds value to the observation.</p>
             </div>
-            <img className="h-40 w-40" src="/Untitled.png" alt="Yellow duck" />
-            <h2>What to record</h2>
-            <p>When making an entry, the most useful things to record include the species name, the amount of birds of that species, the date and the place.
-                Additional information like the age, the sex, the direction of flight or any curious behavior adds value to the observation.</p>
-        </>
+        </div>
     )
 }
 export default RecordThis;
