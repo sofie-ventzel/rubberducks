9c085c32e98556860413493af700978557a00f47
Sofie Ventzel <115930783+sofie-ventzel@users.noreply.github.com>
2023-02-22 13:19:22 +0000

    made the navbar links go larger on hover

--- src/Componants/Header.js
+++ src/Componants/Header.js
@@ -8,19 +8,19 @@ function Header(props) {
                 <img style={{ width: '10%', height: 'auto', margin: '0 4%' }} src="/UntitledTwo.png" alt="Yellow duck" />
                 <h1 style={{ fontSize: '35px' }}>RubberDucks </h1>
                 <nav style={{ flexDirection: 'column', margin: '0 4%' }}>
-                    <NavLink style={{ fontWeight: '600', color: 'black' }} to="/">
+                    <NavLink style={{ fontWeight: '600', color: 'black' }} class="hover:text-lg" to="/">
                         Home
                     </NavLink>
-                    <NavLink style={{ fontWeight: '600', color: 'black' }} to="/search">
+                    <NavLink style={{ fontWeight: '600', color: 'black' }} class="hover:text-lg" to="/search">
                         Search
                     </NavLink>
-                    <NavLink style={{ fontWeight: '600', color: 'black' }} to="/form">
+                    <NavLink style={{ fontWeight: '600', color: 'black' }} class="hover:text-lg" to="/form">
                         Post
                     </NavLink>
-                    <NavLink style={{ fontWeight: '600', color: 'black' }} to="/HowTo">
+                    <NavLink style={{ fontWeight: '600', color: 'black' }} class="hover:text-lg" to="/HowTo">
                         How To Birdwatch
                     </NavLink>
-                    <NavLink style={{ fontWeight: '600', color: 'black' }} to="/Moderator">
+                    <NavLink style={{ fontWeight: '600', color: 'black' }} class="hover:text-lg" to="/Moderator">
                         Moderate
                     </NavLink>
                 </nav>
