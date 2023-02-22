0702b1fa72d1ae48265d13162a26c1591ea9cb4f
Gizem Unlu <unlu.sgizem@gmail.com>
2023-02-22 19:38:16 +0300

    padding and sizing fixes on header

--- src/Componants/Header.js
+++ src/Componants/Header.js
@@ -1,48 +1,55 @@
 import { NavLink } from "react-router-dom";
 function Header(props) {
   return (
-    <header>
+    <header className="p-5">
       <section>
         <img
-          style={{ width: "10%", height: "auto", margin: "0 4%" }}
+          style={{ width: "15%", height: "auto", margin: "0 4%" }}
           src="/UntitledTwo.png"
           alt="Yellow duck"
         />
-        <a className="text-black visited:text-black" href="/">
+        <a style={{}} className=" text-black visited:text-black " href="/">
           <h1 style={{ fontSize: "35px" }}>RubberDucks </h1>
         </a>
-        <nav style={{ flexDirection: "column", margin: "0 4%" }}>
+        <nav
+          style={{
+            width: "15%",
+            height: "200px",
+            flexDirection: "column",
+            margin: "0 4%",
+          }}
+        >
           <NavLink
             style={{ fontWeight: "600", color: "black" }}
-            className="hover:text-lg"
+            className="hover:text-lg p-2"
             to="/"
           >
             Home
           </NavLink>
           <NavLink
             style={{ fontWeight: "600", color: "black" }}
-            className="hover:text-lg"
+            className="hover:text-lg p-2"
             to="/search"
           >
             Search
           </NavLink>
           <NavLink
             style={{ fontWeight: "600", color: "black" }}
-            className="hover:text-lg"
+            className="hover:text-lg p-2"
             to="/form"
           >
             Post
           </NavLink>
           <NavLink
             style={{ fontWeight: "600", color: "black" }}
-            className="hover:text-lg"
+            className="hover:text-lg p-2"
             to="/HowTo"
           >
             How To Birdwatch
           </NavLink>
           <NavLink
             style={{ fontWeight: "600", color: "black" }}
-            className="hover:text-lg"
+            className="hover:text-lg p-2"
             to="/Moderator"
           >
             Moderate
